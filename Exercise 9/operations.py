16d69db6400ee331fd5b63fd08b3e946890c5fe2
Jody Broad <msjodybroad@gmail.com>
2022-01-30 16:15:27 +0000

    added content to most files

---
+++ Exercise 9/operations.py
@@ -0,0 +1,10 @@
+var = input("Please enter a value:")
+
+# using the method upper on the input from var to make it uppercase and then printing it
+print(var.upper())
+
+# how to get it to count the number of letters in the input?
+#print(var.count())
+
+# checks if the input contains numeric data
+print(var.isdecimal())
